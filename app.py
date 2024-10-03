e453aca79889f52201b0c472e26732c4c072453a
hsurapur <59188794+hsurapur@users.noreply.github.com>
2024-10-03 16:09:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -29,17 +29,17 @@ geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92,21)
 balance = st.number_input('Balance')
-#credit_score = st.number_input('Credit Score')
-estimated_salary = st.number_input('Estimated Salary')
+credit_score = st.number_input('Credit Score')
+#estimated_salary = st.number_input('Estimated Salary')
 tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
 
 # Prepare the input data
-geography is not None:  # Only proceed if geography is selected
 input_data = pd.DataFrame({
     'CreditScore': [credit_score],
+    'Geography': [geography],
     'Gender': [label_encoder_gender.transform([gender])],
     'Age': [age],
     'Tenure': [tenure],
@@ -47,7 +47,7 @@ input_data = pd.DataFrame({
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
+    #'EstimatedSalary': [estimated_salary]
 })
 
 # One-hot encode 'Geography'
